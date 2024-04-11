bfb48e769f2c157c60014aadd04a8bf4944e6984
Cianuro-83 <buzzeosimone@yahoo.com>
2024-04-11 16:21:12 +0200

    INIT JETSTREAM +LIVEWIRE+SASS

---
+++ cianuro/resources/views/components/authentication-card-logo.blade.php
@@ -0,0 +1,1 @@
+<img src="{{ asset('storage/img/SIMPSONS 2A.jpg')}}" alt="" width="50px" height="50px">
--- prova/resources/views/components/application-mark.blade.php
+++
@@ -1,1 +0,0 @@
-<img src="{{ asset('storage/img/GLASS 1.jpg') }}" alt="" width="50px" height="50px">

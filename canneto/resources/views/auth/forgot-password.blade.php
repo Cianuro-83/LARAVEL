9bd8227b5979adec71744bca32895319f5655a25
Cianuro-83 <buzzeosimone@yahoo.com>
2024-07-04 22:30:09 +0200

    refactor: :zap: tradotta la pagina forgot password

--- canneto/resources/views/auth/forgot-password.blade.php
+++ canneto/resources/views/auth/forgot-password.blade.php
@@ -4,8 +4,8 @@
             <x-authentication-card-logo />
         </x-slot>
 
-        <div class="mb-4 text-sm text-gray-600 dark:text-gray-400">
-            {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
+        <div class="mb-4 text-sm text-gray-600 dark:text-gray-400 text-justify">
+            {{ __('Hai dimerciato la password? Nessun problema. Inserisci la tua email e ti invieremo un link di reset che ticonsentirà di crearne una nuova.') }}
         </div>
 
         @session('status')
@@ -26,7 +26,7 @@
 
             <div class="flex items-center justify-end mt-4">
                 <x-button>
-                    {{ __('Email Password Reset Link') }}
+                    {{ __('Invia richiesta nuova password') }}
                 </x-button>
             </div>
         </form>
